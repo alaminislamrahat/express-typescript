e0698a6052b932a4368c0423fd312f46c8bbb3f6
alaminislamrahat <alaminislamrahat@gmail.com>
2026-02-23 18:00:56 +0600

    middle ware created

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { Pool } from "pg";
 import dotenv from "dotenv"
 import path from "path"
@@ -42,9 +42,16 @@ const initDb = async () => {
   `);
 };
 
+//middle ware
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}`);
+  next()
+}
 
 
-app.get("/", (req: Request, res: Response) => {
+
+app.get("/", logger,(req: Request, res: Response) => {
   res.send("Hello next level developers!");
 });
 
@@ -184,6 +191,54 @@ app.delete("/users/:id", async(req: Request, res: Response) => {
   }
 })
 
+
+//todos crud
+//post
+app.post("/todos", async(req: Request, res: Response) => {
+  const {user_id, title} = req.body;
+  try{
+    const result = await pool.query(`INSERT INTO todos(user_id, title) VALUES($1, $2) RETURNING *`,[user_id, title]);
+    res.status(201).json({
+      success: true,
+      message: "Data inserted",
+      data: result.rows[0]
+    })
+  }catch(err: any){
+    res.status(500).json({
+      success: false,
+      message: err.message
+    })
+  }
+})
+
+//get route
+app.get("/todos", async(req: Request, res: Response) => {
+  try{
+    const result = await pool.query(`SELECT * FROM todos`);
+    res.status(200).json({
+      success: true,
+      message: "Todos retrived successfully",
+      data: result.rows
+    })
+
+  }catch(err:any){
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      details: err
+    })
+  }
+})
+
+
+
+app.use((req: Request, res:Response) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+    path: req.path
+  })
+})
 
 const start = async () => {
   try {
